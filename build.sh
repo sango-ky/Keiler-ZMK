01db6a3f239acefb3b27acab3c360070ac9f6e38
jonathanforking <jonathanforking@proton.me>
2024-06-30 14:10:12 +0200

    Fixed caps_word combo

--- build.sh
+++ build.sh
@@ -13,7 +13,7 @@ west build -s zmk/app -p -b seeeduino_xiao_ble -- -DSHIELD="Keiler_dongle" -DZMK
 cp build/zephyr/zmk.uf2 "./firmware/${TIMESTAMP}_dongle.uf2"
 
 west build -s zmk/app -p -b nice_nano_v2 -- -DSHIELD="settings_reset" -DZMK_CONFIG="${PWD}/config"
-cp build/zephyr/zmk.uf2 "./firmware/${TIMESTAMP}_nn_reset.uf2"
+cp build/zephyr/zmk.uf2 "./firmware/nn_reset.uf2"
 
 west build -s zmk/app -p -b seeeduino_xiao_ble -- -DSHIELD="settings_reset" -DZMK_CONFIG="${PWD}/config"
-cp build/zephyr/zmk.uf2 "./firmware/${TIMESTAMP}_xia_reset.uf2"
+cp build/zephyr/zmk.uf2 "./firmware/xia_reset.uf2"
