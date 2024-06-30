c2a4425c0ce743e0297fdcc767f732f1a10ffe35
jonathanforking <jonathanforking@proton.me>
2024-06-30 13:56:41 +0200

    Added settings_reset

--- build.sh
+++ build.sh
@@ -11,3 +11,9 @@ cp build/zephyr/zmk.uf2 "./firmware/${TIMESTAMP}_right.uf2"
 
 west build -s zmk/app -p -b seeeduino_xiao_ble -- -DSHIELD="Keiler_dongle" -DZMK_CONFIG="${PWD}/config"
 cp build/zephyr/zmk.uf2 "./firmware/${TIMESTAMP}_dongle.uf2"
+
+west build -s zmk/app -p -b nice_nano_v2 -- -DSHIELD="settings_reset" -DZMK_CONFIG="${PWD}/config"
+cp build/zephyr/zmk.uf2 "./firmware/${TIMESTAMP}_nn_reset.uf2"
+
+west build -s zmk/app -p -b seeeduino_xiao_ble -- -DSHIELD="settings_reset" -DZMK_CONFIG="${PWD}/config"
+cp build/zephyr/zmk.uf2 "./firmware/${TIMESTAMP}_xia_reset.uf2"
